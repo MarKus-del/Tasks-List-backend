1a99763f3751f8487bebc3294a69ee030aa0976e
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-01-22 13:41:42 -0300

    criando metodos no userservice

--- src/main/java/com/markusdel/todolist/service/UserService.java
+++ src/main/java/com/markusdel/todolist/service/UserService.java
@@ -16,4 +16,25 @@ public class UserService {
     public User createUser(User user) {
         return userRepository.save(user);
     }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public User updateUser(Long id, User user) {
+        User one = userRepository.getOne(id);
+        User userUpdated = User.builder()
+                .id(one.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .lastName(user.getLastName())
+                .password(user.getPassword())
+                .build();
+
+        return userRepository.save(userUpdated);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
