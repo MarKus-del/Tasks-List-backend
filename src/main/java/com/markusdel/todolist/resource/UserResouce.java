4039798c7c0f2cc3775458a0087cef830a5cdb2f
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-01-22 13:47:49 -0300

    adicionando metodos do reosurce

--- src/main/java/com/markusdel/todolist/resource/UserResouce.java
+++ src/main/java/com/markusdel/todolist/resource/UserResouce.java
@@ -2,6 +2,7 @@ package com.markusdel.todolist.resource;
 
 import com.markusdel.todolist.model.User;
 import com.markusdel.todolist.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,5 +18,21 @@ public class UserResouce {
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User findUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
