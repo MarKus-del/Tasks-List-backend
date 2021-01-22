6e1325353aa0f8662cfab44bb5dea7e8dfa2be77
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-01-22 14:31:22 -0300

    criando mapper para transformar dto para entity

---
+++ src/main/java/com/markusdel/todolist/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.markusdel.todolist.mapper;
+
+import com.markusdel.todolist.dto.UserCreateDTO;
+import com.markusdel.todolist.dto.UserResponseDTO;
+import com.markusdel.todolist.model.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public User toEntity (UserCreateDTO userCreateDTO) {
+        return  MODEL_MAPPER.map(userCreateDTO, User.class);
+    }
+
+    public UserResponseDTO toUserResponseDTO(User user) {
+        return MODEL_MAPPER.map(user, UserResponseDTO.class);
+    }
+}
